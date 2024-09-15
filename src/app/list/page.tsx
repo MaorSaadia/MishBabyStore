476a757a97d60185117b177844645c10443cac9d
Maor <maorsa9@gmail.com>
2024-09-15 20:15:11 +0300

    fetches the CategoryList from Wix

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import Image from "next/image";
 
-import Filter from "@/components/Filter";
+import { wixClientServer } from "@/lib/wixClientServer";
 import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import Filter from "@/components/Filter";
 
 const ListPage = async ({ searchParams }: { searchParams: any }) => {
-  searchParams.cat || "all-products";
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
@@ -26,8 +33,17 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">For You!</h1>
-      <ProductList />
+      <h1 className="mt-12 text-xl font-semibold">
+        {cat?.collection?.name} For You!
+      </h1>
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
