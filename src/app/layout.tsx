338db4c664949539367104040d84c99987c7b475
Maor <maorsa9@gmail.com>
2024-09-15 19:51:13 +0300

    connect the website to Wix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
+import { WixClientContextProvider } from "@/context/wixContext";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <WixClientContextProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </WixClientContextProvider>
       </body>
     </html>
   );
