b42f315b8756958059ede0aaa3fd1f4cff2dfe61
Maor <maorsa9@gmail.com>
2024-09-19 18:18:33 +0300

    change at the cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -31,10 +31,12 @@ const CartModal: React.FC<CartModalProps> = ({ onClose }) => {
     };
     document.addEventListener("mousedown", handleClickOutside);
 
+    // Disable scrolling on the body when the modal is open
     document.body.style.overflow = "hidden";
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      // Re-enable scrolling when the modal is closed
       document.body.style.overflow = "unset";
     };
   }, [onClose]);
@@ -65,122 +67,117 @@ const CartModal: React.FC<CartModalProps> = ({ onClose }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
-      <div className="relative w-full max-w-md">
-        <button
-          onClick={onClose}
-          className="absolute -top-14 left-1/2 p-2 bg-white rounded-full shadow-md hover:bg-gray-100 transition-colors duration-200"
-        >
-          <X size={24} />
-        </button>
-        <div
-          ref={modalRef}
-          className="bg-white rounded-lg shadow-lg overflow-hidden z-50"
-        >
-          <div className="p-4">
-            <div className="mb-4">
+      <div className="bg-white rounded-lg shadow-lg w-full max-w-md">
+        <div className="p-4">
+          <div className="flex items-center p-2 rounded-t justify-center relative">
+            <button
+              onClick={onClose}
+              className="p-1 transition absolute left-9 rounded-full hover:opacity-70 mb-4"
+            >
+              <X size={24} />
+            </button>
+            <div className="flex items-center justify-between mb-4">
               <h2 className="text-xl font-semibold">Shopping Cart</h2>
             </div>
-            {!cart.lineItems ? (
-              <div className="text-center py-8">Cart is Empty</div>
-            ) : (
-              <>
-                <div className="max-h-[60vh] overflow-y-auto mb-4">
-                  {cart.lineItems.map((item) => (
-                    <div
-                      key={item._id}
-                      className="flex gap-4 mb-4 pb-4 border-b"
-                    >
-                      {item.image && (
-                        <Image
-                          src={wixMedia.getScaledToFillImageUrl(
-                            item.image,
-                            72,
-                            96,
-                            {}
-                          )}
-                          alt=""
-                          width={72}
-                          height={96}
-                          className="object-cover rounded-md cursor-pointer"
-                          onClick={() =>
-                            router.push(
-                              `/${item.productName?.original?.toLowerCase()}`
-                            )
-                          }
-                        />
-                      )}
-                      <div className="flex flex-col justify-between w-full">
-                        <div>
-                          <div className="flex items-start justify-between gap-2">
-                            <h3 className="font-semibold">
-                              {item.productName?.original}
-                            </h3>
-                            <div className="p-1 bg-gray-50 rounded-sm text-sm whitespace-nowrap">
-                              {item.quantity && item.quantity > 1 && (
-                                <span className="text-green-500 mr-1">
-                                  {item.quantity} x{" "}
-                                </span>
-                              )}
-                              ${item.price?.amount}
-                            </div>
-                          </div>
-                          <div className="text-sm text-gray-500">
-                            {item.availability?.status}
+          </div>
+
+          {!cart.lineItems ? (
+            <div className="text-center py-8">Cart is Empty</div>
+          ) : (
+            <>
+              <div className="max-h-[60vh] overflow-y-auto mb-4">
+                {cart.lineItems.map((item) => (
+                  <div key={item._id} className="flex gap-4 mb-4 pb-4 border-b">
+                    {item.image && (
+                      <Image
+                        src={wixMedia.getScaledToFillImageUrl(
+                          item.image,
+                          72,
+                          96,
+                          {}
+                        )}
+                        alt=""
+                        width={72}
+                        height={96}
+                        className="object-cover rounded-md cursor-pointer"
+                        onClick={() =>
+                          router.push(
+                            `/${item.productName?.original?.toLowerCase()}`
+                          )
+                        }
+                      />
+                    )}
+                    <div className="flex flex-col justify-between w-full">
+                      <div>
+                        <div className="flex items-start justify-between gap-2">
+                          <h3 className="font-semibold">
+                            {item.productName?.original}
+                          </h3>
+                          <div className="p-1 bg-gray-50 rounded-sm text-sm whitespace-nowrap">
+                            {item.quantity && item.quantity > 1 && (
+                              <span className="text-green-500 mr-1">
+                                {item.quantity} x{" "}
+                              </span>
+                            )}
+                            ${item.price?.amount}
                           </div>
                         </div>
-                        {item.descriptionLines?.[0]?.colorInfo?.original && (
-                          <div className="text-slate-500 text-xs">
-                            Color: {item.descriptionLines[0].colorInfo.original}
-                          </div>
-                        )}
-                        <div className="flex justify-between text-sm mt-2">
-                          <span className="text-slate-800">
-                            Qty. {item.quantity}
-                          </span>
-                          <span
-                            className="text-rose-500"
-                            style={{
-                              cursor: isLoading ? "not-allowed" : "pointer",
-                            }}
-                            onClick={() => removeItem(wixClient, item._id!)}
-                          >
-                            Remove
-                          </span>
+                        <div className="text-sm text-gray-500">
+                          {item.availability?.status}
                         </div>
                       </div>
+                      {item.descriptionLines?.[0]?.colorInfo?.original && (
+                        <div className="text-slate-500 text-xs">
+                          Color: {item.descriptionLines[0].colorInfo.original}
+                        </div>
+                      )}
+                      <div className="flex justify-between text-sm mt-2">
+                        <span className="text-slate-800">
+                          Qty. {item.quantity}
+                        </span>
+                        <span
+                          className="text-rose-500"
+                          style={{
+                            cursor: isLoading ? "not-allowed" : "pointer",
+                          }}
+                          onClick={() => removeItem(wixClient, item._id!)}
+                        >
+                          Remove
+                        </span>
+                      </div>
                     </div>
-                  ))}
-                </div>
-                <div>
-                  <div className="flex items-center justify-between font-semibold mb-2">
-                    <span>Subtotal</span>
-                    {
-                      // @ts-ignore
-                      <span>${cart.subtotal.amount}</span>
-                    }
                   </div>
-                  <p className="text-gray-500 text-sm mb-4">
-                    Shipping and taxes calculated at checkout.
-                  </p>
-                  <div className="flex flex-col gap-2">
-                    <button
-                      className="w-full rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
-                      disabled={isLoading}
-                      onClick={handleCheckout}
-                    >
-                      Checkout
-                    </button>
-                    <button
-                      className="w-full rounded-md py-3 px-4 ring-1 ring-gray-300"
-                      onClick={() => router.push("/cart")}
-                    >
-                      View Cart
-                    </button>
-                  </div>
+                ))}
+              </div>
+              <div>
+                <div className="flex items-center justify-between font-semibold mb-2">
+                  <span>Subtotal</span>
+                  {
+                    // @ts-ignore
+                    <span>${cart.subtotal.amount}</span>
+                  }
                 </div>
-              </>
-            )}
-          </div>
+                <p className="text-gray-500 text-sm mb-4">
+                  Shipping and taxes calculated at checkout.
+                </p>
+                <div className="flex flex-col gap-2">
+                  <button
+                    className="w-full rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
+                    disabled={isLoading}
+                    onClick={handleCheckout}
+                  >
+                    Checkout
+                  </button>
+                  <button
+                    className="w-full rounded-md py-3 px-4 ring-1 ring-gray-300"
+                    onClick={() => router.push("/cart")}
+                  >
+                    View Cart
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
