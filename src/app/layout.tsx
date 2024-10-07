82fe5222687679dee0805a8568141909ef025dfd
Maor <maorsa9@gmail.com>
2024-10-07 13:51:00 +0300

    add Analytics on vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { WixClientContextProvider } from "@/context/wixContext";
 import Navbar from "@/components/Navbar";
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <WixClientContextProvider>
+          <Analytics />
           <LoginModal />
           <RegisterModal />
           <ResetPasswordModal />
