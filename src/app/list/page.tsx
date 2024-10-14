f5dfef6057347409d65fc0423d8339a26a8ebc0d
Maor <maorsa9@gmail.com>
2024-10-14 19:15:21 +0300

    costumize all the catgories lish add a descriptions and more

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-
 import { wixClientServer } from "@/lib/wixClientServer";
 import ProductList from "@/components/ProductList";
 import Skeleton from "@/components/Skeleton";
@@ -14,15 +13,17 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-      {/* CAMPAIGN */}
-      <div className="hidden bg-gray-50 px-4 sm:flex justify-between h-64">
-        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
-          <h1 className="text-5xl font-semibold leading-[48px] text-gray-700">
-            Grab up to 50% off on
-            <br /> Selected Products
-          </h1>
+      {/* CATEGORY DESCRIPTION */}
+      {cat.collection?.description && (
+        <div className="my-8 p-8 bg-gradient-to-r from-sky-200 via-cyan-100 to-slate-50 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300">
+          <h2 className="text-4xl font-extrabold mb-6 text-slate-600 tracking-wide">
+            {cat.collection.name}
+          </h2>
+          <p className="text-gray-600 text-lg leading-loose whitespace-pre-wrap">
+            {cat.collection.description}
+          </p>
         </div>
-      </div>
+      )}
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
