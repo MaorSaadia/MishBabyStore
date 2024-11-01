bc02ee6220ffc45b8b578218d72017313727484b
Maor <maorsa9@gmail.com>
2024-11-01 13:10:12 +0200

    CATEGORY DESCRIPTION costumzie in mobile screens

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -16,13 +16,13 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CATEGORY DESCRIPTION */}
       {cat.collection?.description && (
-        <div className="my-8 p-8 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden relative">
+        <div className="my-4 md:my-8 p-4 md:p-8 rounded-lg md:rounded-2xl shadow-md hover:shadow-lg md:shadow-lg md:hover:shadow-xl transition-shadow duration-300 overflow-hidden relative">
           <div className="absolute inset-0 bg-gradient-to-r from-sky-200 via-cyan-100 to-slate-50 animate-gradient-x"></div>
           <div className="relative z-10">
-            <h2 className="text-4xl font-extrabold mb-6 text-slate-600 tracking-wide">
+            <h2 className="text-2xl md:text-4xl font-extrabold mb-3 md:mb-6 text-slate-600 tracking-wide">
               {cat.collection.name}
             </h2>
-            <p className="text-gray-600 text-lg leading-loose whitespace-pre-wrap">
+            <p className="text-gray-600 text-base md:text-lg leading-relaxed md:leading-loose whitespace-pre-wrap">
               {cat.collection.description}
             </p>
           </div>
