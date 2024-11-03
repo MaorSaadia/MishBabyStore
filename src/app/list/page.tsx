fd44c03fe37e860e91d3cc2a3fc46209f9205deb
Maor <maorsa9@gmail.com>
2024-11-03 10:09:55 +0200

    create Metadata in the collecnios pages

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,16 +1,50 @@
 import { Suspense } from "react";
-
+import { Metadata } from "next";
 import { wixClientServer } from "@/lib/wixClientServer";
 import ProductList from "@/components/ProductList";
 import Skeleton from "@/components/Skeleton";
 import Filter from "@/components/Filter";
+import { notFound } from "next/navigation";
 
-const ListPage = async ({ searchParams }: { searchParams: any }) => {
+interface PageProps {
+  searchParams: {
+    cat?: string;
+  };
+}
+
+export async function generateMetadata({
+  searchParams,
+}: PageProps): Promise<Metadata> {
+  const wixClient = await wixClientServer();
+  const collection = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
+  if (!collection.collection) {
+    notFound();
+  }
+
+  const banner = collection.collection.media?.mainMedia?.image;
+
+  return {
+    title: collection.collection.name,
+    description: collection.collection.description,
+    openGraph: {
+      images: banner ? [{ url: banner.url }] : [],
+    },
+  };
+}
+
+const ListPage = async ({ searchParams }: PageProps) => {
   const wixClient = await wixClientServer();
 
   const cat = await wixClient.collections.getCollectionBySlug(
     searchParams.cat || "all-products"
   );
+
+  if (!cat.collection) {
+    notFound();
+  }
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
@@ -31,11 +65,11 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">{cat?.collection?.name}</h1>
+      <h1 className="mt-12 text-xl font-semibold">{cat.collection.name}</h1>
       <Suspense fallback={<Skeleton />}>
         <ProductList
           categoryId={
-            cat.collection?._id || "00000000-000000-000000-000000000001"
+            cat.collection._id || "00000000-000000-000000-000000000001"
           }
           searchParams={searchParams}
         />
