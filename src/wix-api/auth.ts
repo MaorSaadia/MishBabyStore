0efdfc87385b96fc0214b878b0535ce9a0149959
Maor <maorsa9@gmail.com>
2025-04-07 12:26:14 +0300

    comments some files

--- src/wix-api/auth.ts
+++ src/wix-api/auth.ts
@@ -1,28 +1,28 @@
-import { WixClient } from "@/context/wixContext";
-import { OauthData } from "@wix/sdk";
+// import { WixClient } from "@/context/wixContext";
+// import { OauthData } from "@wix/sdk";
 
-export async function generateOAuthData(
-  wixClient: WixClient,
-  originPath?: string
-) {
-  return wixClient.auth.generateOAuthData(
-    process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/callback",
-    process.env.NEXT_PUBLIC_BASE_URL + "/" + (originPath || "")
-  );
-}
+// export async function generateOAuthData(
+//   wixClient: WixClient,
+//   originPath?: string
+// ) {
+//   return wixClient.auth.generateOAuthData(
+//     process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/callback",
+//     process.env.NEXT_PUBLIC_BASE_URL + "/" + (originPath || "")
+//   );
+// }
 
-export async function getLoginUrl(wixClient: WixClient, oAuthData: OauthData) {
-  const { authUrl } = await wixClient.auth.getAuthUrl(oAuthData, {
-    responseMode: "query",
-  });
+// export async function getLoginUrl(wixClient: WixClient, oAuthData: OauthData) {
+//   const { authUrl } = await wixClient.auth.getAuthUrl(oAuthData, {
+//     responseMode: "query",
+//   });
 
-  return authUrl;
-}
+//   return authUrl;
+// }
 
-export async function getLogoutUrl(wixClient: WixClient) {
-  const { logoutUrl } = await wixClient.auth.logout(
-    process.env.NEXT_PUBLIC_BASE_URL!
-  );
+// export async function getLogoutUrl(wixClient: WixClient) {
+//   const { logoutUrl } = await wixClient.auth.logout(
+//     process.env.NEXT_PUBLIC_BASE_URL!
+//   );
 
-  return logoutUrl;
-}
+//   return logoutUrl;
+// }
