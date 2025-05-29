2b88824375ae45770ae1902310a3261ff39401be
Maor <maorsa9@gmail.com>
2025-05-29 17:50:51 +0300

    small change

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -302,7 +302,7 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
 
       <div className="mt-4 md:mt-8 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl md:text-3xl font-bold mb-4 text-gray-800">
-          Related Items
+          You May Also Like
         </h1>
         <Suspense fallback={<Skeleton />}>
           <ProductList
