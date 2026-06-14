2b188a7ee4cda20a450bf9cf3d08816ac89ce0b4
Maor <maorsa9@gmail.com>
2026-06-14 19:57:52 +0300

    small change

--- src/components/PromoBanner.tsx
+++ src/components/PromoBanner.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const PromoBanner: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
   const promoText =
-    "New customers 🎉: Use code FIRSTBUY10 for 10% off ALL products on your first purchase! | Until 28/02/2026 | Free Shipping";
+    "New customers 🎉: Use code FIRSTBUY10 for 10% off ALL products on your first purchase! | Until 30/06/2026 | Free Shipping";
   // const promoText =
   //   "End of Year Sale 🎉: Enjoy 15% off ALL products with code ENDYEAR15 until 31/12/24! | Free Worldwide Shipping";
 
